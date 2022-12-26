a17bb97ce56ec1f6655e56bd3a65ead0a9c3bccc
laxmantandon <laxmantandon@gmail.com>
2022-12-26 23:39:50 +0700

    misc

--- erevive_whatsapp/public/js/general_ledger.js
+++ erevive_whatsapp/public/js/general_ledger.js
@@ -217,7 +217,7 @@ frappe.query_reports["General Ledger"] = {
     }
 }
 
-erpnext.utils.add_dimensions('General Ledger', 15)
+// erpnext.utils.add_dimensions('General Ledger', 15)
 
 function whatsapp(print_settings) {
     const base_url = frappe.urllib.get_base_url();
