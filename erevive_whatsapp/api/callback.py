4ba2a5243df7c37a2b696a246de55fd9ae39fd50
laxmantandon <laxmantandon@gmail.com>
2024-04-26 21:03:42 +0530

    Update callback.py

--- erevive_whatsapp/api/callback.py
+++ erevive_whatsapp/api/callback.py
@@ -3,14 +3,20 @@ from werkzeug.wrappers import Response
 
 @frappe.whitelist(allow_guest=True)
 def callback():
-    frappe.log_error(frappe.request.data)
-
     if frappe.request.method == "GET":
 
         hubchallange = frappe.form_dict.get("hub.challenge")
-        frappe.log_error(frappe.request.data)
+        # frappe.log_error(frappe.request.data, 'whatsapp_callback_get')
         return Response(hubchallange)
 
     if frappe.request.method == "POST":
-        frappe.log_error(frappe.request.data)
+        try:
+            frappe.get_doc({
+                "doctype": "Whatsapp Callback",
+                "response": frappe.request.data
+            }).insert(ignore_permissions=True)
 
+        except Exception as e:
+            frappe.log_error(e, 'whatsapp_callback')
+        finally:
+            frappe.db.commit()
